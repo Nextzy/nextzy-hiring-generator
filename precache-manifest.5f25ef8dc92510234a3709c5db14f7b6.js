254054abba721e6bf61a55bd675216db8b38b9a6
Akexorcist <akexorcist@gmail.com>
2019-10-15 20:27:18 +0700

    Updates

---
+++ precache-manifest.5f25ef8dc92510234a3709c5db14f7b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7978d53a88a06a916ac",
+    "url": "/nextzy-hiring-generator/static/css/main.3baf61f9.chunk.css"
+  },
+  {
+    "revision": "d7978d53a88a06a916ac",
+    "url": "/nextzy-hiring-generator/static/js/main.6eea39ca.chunk.js"
+  },
+  {
+    "revision": "05ce9c8430e4005ef17d",
+    "url": "/nextzy-hiring-generator/static/js/runtime~main.85bd493e.js"
+  },
+  {
+    "revision": "ff101ef5f164dad1b13d",
+    "url": "/nextzy-hiring-generator/static/css/2.04d61fac.chunk.css"
+  },
+  {
+    "revision": "ff101ef5f164dad1b13d",
+    "url": "/nextzy-hiring-generator/static/js/2.e91d9feb.chunk.js"
+  },
+  {
+    "revision": "a64415bf77594b8088fec77aac8a3810",
+    "url": "/nextzy-hiring-generator/static/media/background.a64415bf.jpg"
+  },
+  {
+    "revision": "2a5ced22cf3b9685b85247728c368e90",
+    "url": "/nextzy-hiring-generator/index.html"
+  }
+];
